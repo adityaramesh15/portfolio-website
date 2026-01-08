1104ee3a325230e2e76180e568d15cba9e2c8696
Aditya Ramesh <adityaramesh15@gmail.com>
2026-01-08 15:01:05 -0600

    Updating Email

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.getElementById("email-link").addEventListener("click", function (e) {
+  e.preventDefault();
+  const user = "me";
+  const domain = "adityaramesh.net";
+  window.location.href = `mailto:${user}@${domain}`;
+});
